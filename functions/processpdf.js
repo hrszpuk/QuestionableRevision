8866b8ed4fe0a12a7a0236785baac82aba0e3119
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-10-26 19:04:34 +0100

    Added processpdf

---
+++ functions/processpdf.js
@@ -0,0 +1,6 @@
+import { initializeApp} from "firebase-admin";
+import { getStorage } from "firebase-admin/storage"
+import logger from ""
+const logger = require("firebase-functions/logger");
+const path = require("path");
+
