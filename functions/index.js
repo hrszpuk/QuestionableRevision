e3eb6bc5b17a981791d4688ec84b1579cd94d0d0
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-10-27 01:35:13 +0100

    Added lobbyStart and lobbyClose to index

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,19 @@
 const { initializeApp } = require("firebase-admin/app");
 
-initializeApp();
+initializeApp({
+    databaseURL: 'https://questionablerevision-default-rtdb.europe-west1.firebasedatabase.app/'
+});
 
 const {echo} = require("./src/echo");
 const {uploadpdf} = require("./src/uploadpdf");
 const {generateQuestionsAndAnswers} = require("./src/generateq&a");
+const {lobbyStart} = require("./src/lobby-start");
+const {lobbyClose} = require("./src/lobby-close");
 
 // Remember to add new functions to the rewrites in the same format as the others.
 exports.echo = echo;
 exports.uploadpdf = uploadpdf;
 exports.generateQuestionsAndAnswers = generateQuestionsAndAnswers;
+exports.lobbyStart = lobbyStart;
+exports.lobbyClose = lobbyClose;
 
