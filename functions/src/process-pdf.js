05ac09abaa2ed38b846be5b3050b0daa79507c74
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-10-27 04:59:25 +0000

    Hooked up Gemini API and generating quizzes works

--- functions/src/process-pdf.js
+++ functions/src/process-pdf.js
@@ -2,12 +2,21 @@ const { onRequest } = require("firebase-functions/v2/https");
 const cors = require("cors")({ origin: true });
 const admin = require("firebase-admin");
 const pdf = require("pdf-parse");
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+const axios = require("axios")
+const {GoogleGenerativeAI} = require("@google/generative-ai")
+
+const structure_specifier = "Your answer should be structured in JSON. The quiz should be an array of objects with each object being a question. Each question object should contain a “question” field containing the question, an “options” field containing an array of 4 options, and the “answer” field. The question and options should NOT be prefixed with numbers or letters. The final object should be called \\“questionSet\\”"
+const prompt = "Generate a multiple choice quiz using the content provided below.\n\n" + structure_specifier
+
 
 // Takes a {"filename":"example.pdf"} and needs that file to be in storage to work
 exports.processPdf = onRequest({ region: 'europe-west1' }, async (req, res) => {
     cors(req, res, async () => {
         try {
-            const filename = req.body.filename
+            const {filename} = req.body;
 
             if (!filename) {
                 return res.status(400).send('No filename provided');
@@ -24,9 +33,30 @@ exports.processPdf = onRequest({ region: 'europe-west1' }, async (req, res) => {
 
             fs.unlinkSync(tempFilePath);
 
+            if (data.text.toString().trim() === "") {
+                res.status(500).send('PDF process resulted in empty text');
+                return
+            }
+
+            const genAI = new GoogleGenerativeAI('AIzaSyD0yqVw9NcjOKCsMUrAy4H8z4y9qeTku68');
+            const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+            const result = await model.generateContent(prompt + '\n\n' + data.text);
+
+            let questionSet = result.response.text();
+            questionSet = questionSet
+                .replace("```json", "")
+                .replace("```", "");
+            let questionSetJson = JSON.parse(questionSet);
+
+            if (questionSet === "[GoogleGenerativeAI Error]" || questionSet === "undefined") {
+                res.status(500).send('AI Error:' + questionSet);
+                return
+            }
+
             res.status(200).json({
-                message: 'PDF processed successfully',
-                extractedText: data.text
+                message: 'PDF processed and questions generated successfully',
+                questions: questionSetJson,
             });
 
         } catch (error) {
