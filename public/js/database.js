e7a5c0b6652a5a73889bfd594366a40f18ccb930
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-10-27 07:52:24 +0000

    Added database.js

---
+++ public/js/database.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKYVLgjQGiIi1hNAhfMBH5ht7diUIFxbU",
+    projectId: "questionablerevisio",
+    storageBucket: "gs://questionablerevision.appspot.com",
+    messagingSenderId: "502105386254 ",
+    appId: "1:502105386254:web:f0fa77c4fe251ec0843b5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
