67a1f0c61bf8e0b1d6ada575ea4e85147541e824
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-10-27 09:22:33 +0000

    Added firebase initialisation

---
+++ public/js/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+import "../index.css";
+import "../background.css";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKYVLgjQGiIi1hNAhfMBH5ht7diUIFxbU",
+    projectId: "questionablerevisio",
+    storageBucket: "gs://questionablerevision.appspot.com",
+    messagingSenderId: "502105386254 ",
+    appId: "1:502105386254:web:f0fa77c4fe251ec0843b5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db }
+
